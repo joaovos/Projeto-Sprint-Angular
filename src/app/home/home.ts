b47d0c2d0555b2bfef60dfeb76b2c3c335540950
João Vitor <joao.jvos22@gmail.com>
2026-09-03 11:43:50 -0300

    criação do menu-hamburguer

--- src/app/home/home.ts
+++ src/app/home/home.ts
@@ -7,5 +7,9 @@ import { Component } from '@angular/core';
   styleUrl: './home.css',
 })
 export class Home {
+  isMenuOpen = false;
 
+  toggleMenu(): void {
+    this.isMenuOpen = !this.isMenuOpen;
+  }
 }
